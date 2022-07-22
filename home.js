e142561a668cbac213c16b22fa5970c58f54d7d0
AnuGeorge01 <Anuannah001@gmail.com>
2022-07-22 19:29:55 +0530

    first commit

--- home.js
+++ home.js
@@ -11,3 +11,18 @@ function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden'
     
 }
+
+
+const cityUrl = "https://zomatoajulypi.herokuapp.com/location";
+const getCity = async() => {
+    let response = await fetch(cityUrl,{method:'GET'})
+    let data = await response.json()
+    data.map((item) => {
+        let element = document.createElement('option') //<option></option>
+        let text = document.createTextNode(item.state) // Delhi
+        element.appendChild(text) //<option>Delhi</option>
+        element.value = item.state_id //<option value="1">Delhi</option>
+        document.getElementById('city').appendChild(element)
+        //<select><option value="1">Delhi</option></select>
+    })
+}
